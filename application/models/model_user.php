ed4efdbc3a3d3d6d7537ebb41b4f844f401e207b
kashifhussain000666 <80550972+kashifhussain000666@users.noreply.github.com>
2022-03-09 02:52:49 +0500

    new cchanges

--- application/models/model_user.php
+++ application/models/model_user.php
@@ -211,8 +211,8 @@ class model_user extends CI_Model {
 									    FROM divisiondetails dd
 									    WHERE dd.id = cseventscheduleview.Division
 									) as divisionname,
-									'' as playeridhome, 
-									'' as playeridaway, 
+									playeridhome as playeridhome, 
+									playeridaway as playeridaway, 
 									home, 
 									away, 
 									course, 
